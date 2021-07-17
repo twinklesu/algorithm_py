a4852d85970782da67fcd74224804688e54dd88e
twinklesu <twinklesu914@gmail.com>
2021-07-17 22:18:48 +0900

    미리 ()랑[]제거해서, 숫자가 연속적일 수 있는 경우는 3깊이?까지뿐

--- 2504.py
+++ 2504.py
@@ -1,48 +1,68 @@
 from collections import deque
 
-line = list(map(str, input()))
+
+line = input()
+line = line.replace("()", "2").replace("[]", "3")
 stack = deque()
-stack.append(line[0])
-sett = {'(':')', '[':']'}
-values = {'(': 2, '[': 3}
 
-flag = True
-for l in line[1:]:
-    if l in '([':
-        stack.append(l)
-    elif l in ')]': # ) ]
-        top = stack.pop()
-        if type(top) is int:
-            secondTop = stack.pop()
-            if l == sett[secondTop]:
-                value = values[secondTop]
-                stack.append(top*value)
-            else:
-                flag = False
-                break
+for letter in line:
+    if letter in "([":
+        stack.append(letter)
+    elif letter.isdigit():
+        if stack and stack[-1].isdigit():
+            temp = stack.pop()
+            stack.append(str(int(temp)+int(letter)))
         else:
-            if l in sett and l == sett[top]:
-                value = values[top]
-                stack.append(value)
-            else:
-                flag = False
+            stack.append(letter)
+    elif letter == ")":
+        if stack:
+            if stack[-1].isdigit():
+                temp = stack.pop()
+                if stack and stack[-1] == "(":
+                    stack.pop()
+                    if stack and stack[-1].isdigit():
+                        temp2 = stack.pop()
+                        stack.append(str(int(temp)*2 + int(temp2)))
+                    else:
+                        stack.append(str(int(temp)*2))
+                else: # [ 인 경우, 빈 스택인 경우
+                    print(0)
+                    break
+            elif stack[-1] == "(":
+                stack.pop()
+                stack.append("2")
+            else: # [ 인 경우
+                print(0)
                 break
-    else:
-        flag = False
-        break
-    # 덧셈 확인
-    while len(stack)> 1:
-        top = stack.pop()
-        secondTop = stack.pop()
-        if type(top) is int and type(secondTop) is int:
-            stack.append(top+secondTop)
-        else:
-            stack.append(secondTop)
-            stack.append(top)
+        else: # 빈 stack에 닫음
+            print(0)
             break
-
-
-if flag and len(stack) == 1:
-    print(stack[0])
+    elif letter == "]":
+        if stack:
+            if stack[-1].isdigit():
+                temp = stack.pop()
+                if stack and stack[-1] == "[":
+                    stack.pop()
+                    if stack and stack[-1].isdigit():
+                        temp2 = stack.pop()
+                        stack.append(str(int(temp)*3 + int(temp2)))
+                    else:
+                        stack.append(str(int(temp)*3))
+                else: # ( 인 경우, 빈 스택인 경우
+                    print(0)
+                    break
+            elif stack[-1] == "[":
+                stack.pop()
+                stack.append("3")
+            else: # ( 인 경우
+                print(0)
+                break
+        else: # 빈 stack에 닫음
+            print(0)
+            break
 else:
-    print(0)
+    ans = stack.pop()
+    if stack or not ans.isdigit():
+        print(0)
+    else:
+        print(ans)
