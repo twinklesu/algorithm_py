0c81f8206945d7abb31244c050a8fe848a75ffab
twinklesu <twinklesu914@gmail.com>
2021-07-20 13:14:48 +0900

    그냥 gcd하니까 되는데 전에 왜 안됐지?'

--- 1850.py
+++ 1850.py
@@ -1,26 +1,15 @@
+# from math import gcd
+
 def gcd(a, b):
-    while b != 0:
-        a, b = b, a%b
-    return a
-
-def main():
-    aStr, bStr = map(str, input().split())
-    lenA = len(aStr)
-    lenB = len(bStr)
-    n = max(lenA, lenB)
-    aStr = '0'*(n-lenA) + aStr
-    bStr = '0'*(n-lenB) + bStr
-
-    a = list(map(int, aStr))
-    b = list(map(int, bStr))
-    for i in range(n):
-        a[i] = a[i]-b[i]
-    sub = 0
-    for s in a:
-        sub *= 10
-        sub += s
-    sub = abs(sub)
-    print('1'*sub)
+    if b == 0:
+        return a
+    return gcd(b, a%b)
 
 
-main()
+n, m = map(int, input().split())
+gcdNum = 0
+if n <= m:
+    gcdNum = gcd(m, n)
+else:
+    gcdNum = gcd(n, m)
+print('1'*gcdNum)
