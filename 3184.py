a89df9de4b856689c618cc31ada95c3b2636c51a
twinklesu <twinklesu914@gmail.com>
2021-07-26 10:22:10 +0900

    3184 테케 3번 무한루프

---
+++ 3184.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+n, m = map(int, input().split())
+field = [list(map(str, input())) for _ in range(m)]
+
+sheep = set()
+wolves = set()
+space = []
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+for i in range(1, m-1):
+    for j in range(1, n-1): # 어차피 맨 끝은 범위로 생각 안함
+        if field[i][j] == 'o':
+            sheep.add((i,j))
+        elif field[i][j] == 'v':
+            wolves.add((i,j))
+        if field[i][j] != '#':
+            # bfs
+            temp = set()
+            q = deque()
+            q.append([i,j])
+            field[i][j] = '#' # 방문했으니 울타리로 마킹
+            flag = True
+            while q and flag:
+                x, y = map(int, q.popleft())
+                temp.add((x,y))
+                for k in range(4):
+                    newX = x+dx[k]
+                    newY = y+dy[k]
+                    if field[newX][newY] != '#': # 늑대랑 양도 영역으로 세야하니까
+                        if 1<=newX<m-1 and 1<=newY<n-1:
+                            if field[newX][newY] == 'o':
+                                sheep.add((newX, newY))
+                            elif field[newX][newY] == 'v':
+                                wolves.add((newX, newY))
+                            field[newX][newY] = '#'
+                            q.append([newX, newY])
+                        else:
+                            flag = False
+                            break
+            else:
+                space.append(temp)
+
+
+totalS = 0
+totalW = 0
+for oneSpace in space:
+    sCount = len(oneSpace.intersection(sheep))
+    wCount = len(oneSpace.intersection(wolves))
+    if sCount > wCount:
+        totalS += sCount
+    else:
+        totalW += wCount
+
+print(totalS, totalW)
