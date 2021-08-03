052f9bece93a906575fe5a17e11a62c35ffb1e52
twinklesu <twinklesu914@gmail.com>
2021-08-03 14:10:02 +0900

    경우의수를 미리 생각해보자.. 그릴수있는건 그려보자!

--- 2304.py
+++ 2304.py
@@ -1,45 +1,40 @@
 n = int(input())
-poll = []
+polls = []
 maxHeight = 0
-maxHeightX = 0
+maxHeightLastX = 0
 for _ in range(n):
     x, y = map(int, input().split())
-    if y > maxHeight:
-        maxHeightX = x
+    if y >= maxHeight:
         maxHeight = y
-    poll.append([x,y])
+        maxHeightLastX = x
+    polls.append([x, y])
 
-poll.sort()
+polls.sort()
 
-beforeMax = []
-afterMax = []
-
-beforeH = 0
-for p in poll:
-    if p[1] > beforeH:
-        beforeMax.append(p)
-        beforeH = p[1]
-    if p[0] == maxHeightX:
+pollsUntilMax = []
+pollsAfterMax = []
+beforeHeight = 0
+for p in polls:
+    if maxHeightLastX < p[0]:
         break
-
-beforeH = 0
-for p in poll[::-1]:
-    if p[0] == maxHeightX:
-        afterMax.append([p[0]+1, p[1]])
+    if p[1] >= beforeHeight:
+        pollsUntilMax.append(p)
+        beforeHeight = p[1]
+beforeHeight = 0
+for p in polls[::-1]:
+    if p[0] <= maxHeightLastX:
         break
-    if p[1] > beforeH:
-        afterMax.append(p)
-        beforeH = p[1]
+    if p[1] >= beforeHeight:
+        pollsAfterMax.append([p[0]+1, p[1]])
+        beforeHeight = p[1]
 
-afterMax[0][0] += 1 # 기둥의 끝이 필요해서
 
+pollsAfterMax.append([maxHeightLastX+1, maxHeight])
 ans = 0
-for i in range(len(beforeMax)-1):
-    ans += (beforeMax[i+1][0]- beforeMax[i][0]) * beforeMax[i][1]
-ans += maxHeight
-for i in range(len(afterMax)-1):
-    print((afterMax[i][0]- afterMax[i+1][0]) * afterMax[i][1])
-    ans += (afterMax[i][0]- afterMax[i+1][0]) * afterMax[i][1]
-    print(ans)
+for i in range(len(pollsUntilMax)-1):
+    ans += (pollsUntilMax[i+1][0] - pollsUntilMax[i][0]) * pollsUntilMax[i][1]
+ans += maxHeight # 너비 1 짜리
+for i in range(len(pollsAfterMax)-1):
+    ans += (pollsAfterMax[i][0] - pollsAfterMax[i+1][0]) * pollsAfterMax[i][1]
 
 print(ans)
