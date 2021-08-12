7cfd1a472684e1111a2ee06807f61e39574ac10a
twinklesu <twinklesu914@gmail.com>
2021-08-12 19:00:32 +0900

    다시 2차원 list bfs로 revert

--- 2589.py
+++ 2589.py
@@ -1,27 +1,24 @@
 from collections import deque
 from copy import deepcopy
-import sys
-input = sys.stdin.readline
 
 
 n, m = map(int, input().split())
-mapp = set()
+mapp = [[1 for _ in range(m)] for _ in range(n)] # 1이면 땅, 0이면 바다
 land = deque()
 for i in range(n):
     line = input()
     for j, el in enumerate(line):
         if el == 'L':
-            mapp.add((i,j))
+            mapp[i][j] = 0
             land.append([i,j])
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 # 모든 땅 좌표에 대해 bfs 시행
 ans = 0
-visited = set()
 for x, y in land:
-    visited.clear()
-    visited.add((x,y)) # visited
+    thisMap = deepcopy(mapp)
+    thisMap[x][y] = 1 # visited
     q = deque()
     q.append([x,y,0]) # x, y, depth
     thisCount = 0
@@ -31,8 +28,8 @@ for x, y in land:
         for i in range(4):
             newX = xx + dx[i]
             newY = yy + dy[i]
-            if (newX, newY) not in visited and (newX, newY) in mapp:
-                visited.add((newX, newY))
+            if 0<=newX<n and 0<=newY<m and thisMap[newX][newY] == 0:
+                thisMap[newX][newY] = 1
                 q.append([newX, newY, depth+1])
 
     ans = max(ans, thisCount)
