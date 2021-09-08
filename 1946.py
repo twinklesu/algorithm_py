2600b57823c2392c7e940092cafd69127515f122
twinklesu <twinklesu914@gmail.com>
2021-09-08 23:59:28 +0900

    dp

---
+++ 1946.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    count = 1
+    n = int(input())
+    candidates = [0 for _ in range(n+1)]
+    for _ in range(n):
+        ind, el = map(int, input().split())
+        candidates[ind] = el
+    cutLine = candidates[1]
+    for i in range(2, n+1):
+        if candidates[i] < cutLine:
+            count += 1
+            cutLine = candidates[i]
+
+    print(count)
+    
--- 2565.py
+++ 2565.py
@@ -1,20 +1,20 @@
-import sys
-input = sys.stdin.readline
+from heapq import heappush, heappop
+n = int(input())
+li = []
+for _ in range(n):
+    u, v = map(int, input().split())
+    heappush(li, [u, v])
 
-t = int(input())
-for _ in range(t):
-    count = 1
-    n = int(input())
-    candidates = [0 for _ in range(n+1)]
-    for _ in range(n):
-        ind, el = map(int, input().split())
-        candidates[ind] = el
-    cutLine = candidates[1]
-    for i in range(2, n+1):
-        if candidates[i] < cutLine:
-            count += 1
-            cutLine = candidates[i]
+line = []
+while li:
+    pri, value = heappop(li)
+    line.append(value)
 
-    print(count)
+# 가장 긴 증가하는 수열 dp 로 찾으면 됨
+dp = [1 for _ in range(n)]
+for i in range(n):
+    for j in range(0,i):
+        if line[i] > line[j] and dp[j]+1 > dp[i]:
+            dp[i] = dp[j] + 1
 
-
+print(n-max(dp))
