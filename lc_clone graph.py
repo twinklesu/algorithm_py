b15f4affbf98f7d432192410845fb5ce94199b0f
twinklesu <twinklesu914@gmail.com>
2022-01-10 21:33:00 +0900

    clone graph

--- lc_clone graph.py
+++ lc_clone graph.py
@@ -8,51 +8,47 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
+    def __str__(self):
+        result = "val: "
+        result += str(self.val)
+        result += ", neighbors: "
+        for n in self.neighbors:
+            result += str(n.val)
+            result += ", "
+        return result
+
+
 def cloneGraph(node: 'Node') -> 'Node':
     if node:
-        graph = [[] for _ in range(101)]
+        nodeList = [Node(i) for i in range(101)]
         visited = [False for _ in range(101)]
         visited[0] = True  # place holder
         visited[node.val] = True
         stack = deque()
         stack.append(node)
+        nodeList[node.val] = Node(node.val)
 
         while stack:
             thisNode = stack.pop()
             for nextNode in thisNode.neighbors:
-                graph[thisNode.val].append(nextNode.val)
+                nodeList[thisNode.val].neighbors.append(nodeList[nextNode.val])
                 if not visited[nextNode.val]:
                     visited[nextNode.val] = True
                     stack.append(nextNode)
 
-        maxVal = 0
-        for ind, v in enumerate(visited):
-            if not v:
-                maxVal = ind - 1
-                break
-
-        # new node generation
-        nodes = [None]  # place holder
-        for i in range(1, maxVal + 1):
-            nodes.append(Node(val=i))
-
-        headNode = nodes[1]
-        for i in range(1, maxVal+1):
-            for nn in graph[i]:
-                nodes[i].neighbors.append(nodes[nn])
-        return headNode
-
+        return nodeList[node.val]
     else:
         return None
+
 
 one = Node(1)
 two = Node(2)
 three=Node(3)
 four = Node(4)
-one.neighbors = [two]
+one.neighbors = [two, four]
 two.neighbors = [one, three]
 three.neighbors = [two, four]
-four.neighbors = [three]
+four.neighbors = [three, one]
 # one = Node(1)
 #
 # one = Node(1)
@@ -60,3 +56,14 @@ four.neighbors = [three]
 # one.neighbors = [two]
 # two.neighbors = [one]
 answer = cloneGraph(one)
+stack = deque()
+stack.append(answer)
+visited = dict()
+
+while stack:
+    thisNode = stack.pop()
+    print(thisNode)
+    visited[thisNode.val] = True
+    for nn in thisNode.neighbors:
+        if nn.val not in visited:
+            stack.append(nn)
