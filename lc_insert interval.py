9971b20ea6cdd05e76ae76bf1161ff4c18eda887
twinklesu <twinklesu914@gmail.com>
2022-01-16 19:29:05 +0900

    leetcode insert interval success

--- lc_insert interval.py
+++ lc_insert interval.py
@@ -1,43 +1,48 @@
 def insert(intervals, newInterval):
     answer = []
     if intervals:
-        if newInterval[1] < intervals[0][0]:
-            # 새 구간이 현재 구간 아래
-            answer.append(newInterval)
-            answer.extend(intervals)
-            return answer
-        if intervals[-1][1] < newInterval[0]:
-            # 현재 모든 구간이 새 구간의 아래
-            answer.extend(intervals)
-            answer.append(newInterval)
-            return answer
-        # 새 구간이 현재 구간들 사이에 있으며 겹쳐질 수 있음
-        insertInterval = newInterval.copy()
-        for interval in intervals:
-            intervalStart, intervalEnd = interval
-            # break 조건
-            if newInterval[1] < intervalStart:
-                break
-            if intervalStart <= newInterval[0] <= intervalEnd:
-                insertInterval[0] = intervalStart
-            if intervalStart <= newInterval[1] <= intervalEnd:
-                insertInterval[1] = intervalEnd
-        isInserted = False
+        maxi = max(intervals[-1][1], newInterval[1])
+        intervalList = [[False, False] for _ in range(maxi+1)]
         for start, end in intervals:
-            if end < insertInterval[0]:
-                answer.append([start, end])
-            elif insertInterval[1] < start:
-                if not isInserted:
-                    answer.append(insertInterval)
-                    isInserted = False
-                answer.append([start, end])
+            intervalList[start][0] = True # 머리임
+            for i in range(start+1, end):
+                intervalList[i][0] = True # 구간에 포함
+            intervalList[end][1] = True #꼬리임
+        # 새 구간 삽입
+        if newInterval[0] == newInterval[1]:
+            # 한구간이 점일 때,
+            if intervalList[newInterval[0]] == [False, False]:
+                # 그 구간에 값이 없었을 경우
+                intervalList[newInterval[0]] = [True, True]
+            else:
+                pass
+        else:
+            intervalList[newInterval[0]][0] = True #머리
+            intervalList[newInterval[0]][1] = False # 머리
+            for i in range(newInterval[0]+1, newInterval[1]):
+                intervalList[i][0] = True
+                intervalList[i][1] = False
+            # 꼬리가 구간이 아예 아니였을 경우
+            if intervalList[newInterval[1]] == [False, False]:
+                intervalList[newInterval[1]][1] = True # 꼬리로 만들어줌
+
+        temp = []
+        for ind, el in enumerate(intervalList):
+            isHead, isTail = el
+            if isHead and not temp:
+                # 머리이면
+                temp.append(ind)
+            if isTail and temp:
+                # 꼬리이면
+                temp.append(ind)
+                answer.append(temp.copy())
+                temp = []
         return answer
     else:
         return [newInterval]
 
 
 
-intervals = [[3,5],[12,15]]
-
-newInterval = [6,6]
+intervals = [[1,3],[6,9]]
+newInterval = [2,5]
 print(insert(intervals, newInterval))
