c01ebfac540f02e2fa891796f04fcbf25b147d0d
twinklesu <twinklesu914@gmail.com>
2022-01-24 15:56:44 +0900

    lc longest repeating char replacement

--- lc_longestRepeatingCharacterReplcement.py
+++ lc_longestRepeatingCharacterReplcement.py
@@ -1,25 +1,28 @@
 from collections import defaultdict
-def characterReplacement(s: str, k: int) -> int:
-    start, end = 0, 1
-    memory = defaultdict(int)
-    maxiLen = 0
 
-    memory[s[start]] += 1
-    while start <= end < len(s):
-        thisLen = end - start
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        start, end = 0, 1
+        memory = defaultdict(int)
+        maxiLen = 0
+        thisLen = 1
+        lenS = len(s)
+
+        memory[s[start]] += 1
+
+        while end < lenS:
+            if thisLen - max(memory.values()) <= k:
+                # possible to extend
+                maxiLen = max(maxiLen, thisLen)
+                memory[s[end]] += 1
+                end += 1
+                thisLen += 1
+            else:
+                # shrink the length
+                memory[s[start]] -= 1
+                start += 1
+                thisLen -= 1
         if thisLen - max(memory.values()) <= k:
-            # possible to extend
-            maxiLen = max(maxiLen, thisLen)
-            memory[s[end]] += 1
-            end += 1
-        else:
-            # shrink the length
-            memory[s[start]] -= 1
-            start += 1
-    if end - start - max(memory.values()) <= k:
-        maxiLen = max(maxiLen, end-start)
-    return maxiLen
-
-s = "ABAA"
-k = 0
-print(characterReplacement(s,k))
+            maxiLen = max(maxiLen, end - start)
+        return maxiLen
